8761af71f77ca65210c491f71b9f8f69ca350ba2
hensli <634386086@qq.com>
2019-12-18 09:19:24 +0800

    创建文件
    秒杀其中方法

---
+++ Demo1.java
@@ -0,0 +1,8 @@
+public String list(Model model)
+    {
+        //list.jsp+mode=ModelAndView
+        //获取列表页
+        List<Seckill> list=seckillService.getSeckillList();
+        model.addAttribute("list",list);
+        return "list";
+    }
